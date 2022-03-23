b2a53368db60d4d77c28bd0157c8aa3d5c99e5dd
anna <ann.starovoytova@gmail.com>
2022-03-24 10:07:57 +1300

    added React Router and routs for website tabs in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from './ui/Theme'
 import Header from "./ui/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello!
-      {/* {[...new Array(120)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')} */}
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route exact path="/customsoftware" component={() => <div>Custome Software</div>} />
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact Us</div>} />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
