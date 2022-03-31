94ead48389ddf2dd6f3405a11ec2438bd6e44e6e
anna <ann.starovoytova@gmail.com>
2022-04-01 09:53:29 +1300

    fixed the highlight of services tab when active, added services menu item to menu to be able to return to general services page

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -155,9 +155,30 @@ export default function Header(props) {
                   onClose={handleClose}
                   MenuListProps={{onMouseLeave: handleClose}}
                 >
-                  <MenuItem onClick={handleClose} component={Link} to="/customsoftware">Custom Software Development</MenuItem>
-                  <MenuItem onClick={handleClose} component={Link} to="/mobileapps">Mobile App Development</MenuItem>
-                  <MenuItem onClick={handleClose} component={Link} to="/websites">Website Software Development</MenuItem>
+                  <MenuItem 
+                    onClick={() => {handleClose(); setValue(1)}} 
+                    component={Link} to="/services"
+                  >
+                    Services
+                  </MenuItem>
+                  <MenuItem 
+                    onClick={() => {handleClose(); setValue(1)}} 
+                    component={Link} to="/customsoftware"
+                  >
+                    Custom Software Development
+                  </MenuItem>
+                  <MenuItem 
+                    onClick={() => {handleClose(); setValue(1)}} 
+                    component={Link} to="/mobileapps"
+                  >
+                    Mobile App Development
+                  </MenuItem>
+                  <MenuItem 
+                    onClick={() => {handleClose(); setValue(1)}} 
+                    component={Link} to="/websites"
+                  >
+                    Website Software Development
+                  </MenuItem>
                 </Menu>
               </Toolbar>
           </AppBar>
